a3640c1a00029da038b809572259c41cdcd6e792
prashant-humney <prashant.humney89@gmail.com>
2021-03-06 11:18:45 +0530

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Prashant Humney on 06/03/21.
+//  Copyright © 2021 Prashant Humney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
